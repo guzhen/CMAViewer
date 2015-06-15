9b826f585914d1498a180db8ebb3226fbb4a5a12
guzhen <talentgz2004@hotmail.com>
2015-06-15 08:57:13 +0800

    separate file

--- cmaviewer.rb
+++ cmaviewer.rb
@@ -1,41 +1,5 @@
 require 'qt'
-
-class CMAApp
-	FOLDERS=["app","appmeta","license","patch","savedata","sce_sys",]
-	PATH='APP'
-
-	def initialize
-		@info={'path'=>'','user'=>[]}
-		@data={}
-	end
-
-	def load(path)
-		begin
-			Dir.chdir path do
-				@info[:path]=path
-				Dir.chdir PATH
-				@info[:user]=Dir.entries.select{|e| e!='.' && e!='..'}
-				return @info[:user]
-			end
-		rescue
-			return nil
-		end
-	end
-
-	def select(user)
-		if !@info[:user].include?(user)
-			return nil
-		end
-		if @data[user]!=nil
-			return @data[user]
-		end
-		begin
-			Dir.chdir @info[path]
-		rescue
-			return nil
-		end
-	end
-end
+require_relative 'cmaloader.rb'
 
 class UIApp<Qt::Widget
 
